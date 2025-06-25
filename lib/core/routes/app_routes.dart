db35504830c7187d3adf8ace4d0f6c92d9a2e845
Prijesh Bikram Shahi <prijeshshahi0@gmail.com>
2025-06-25 20:20:56 +0545

    routing fixed

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -2,14 +2,11 @@ import 'package:get/get.dart';
 import '../../views/pages/home_page.dart';
 import '../../views/pages/add_edit_task_page.dart';
 
-/// Centralized route management for the application
-/// Defines all routes and their corresponding pages
 class AppRoutes {
   static const String home = '/';
   static const String addTask = '/add-task';
   static const String editTask = '/edit-task';
 
-  /// List of all application routes
   static final routes = [
     GetPage(
       name: home,
